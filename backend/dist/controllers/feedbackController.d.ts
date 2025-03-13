fa394473c659eaa153c23947748e362649703c31
anurag-kochar <anuragmarketing101@gmail.com>
2025-03-14 05:14:21 +0530

    chores: remove dist

--- backend/dist/controllers/feedbackController.d.ts
+++
@@ -1,8 +0,0 @@
-import { Response } from "express";
-import { AuthenticatedRequest } from "../types";
-export declare const createFeedback: (req: AuthenticatedRequest, res: Response) => Promise<Response<any, Record<string, any>> | undefined>;
-export declare const getFeedback: (req: AuthenticatedRequest, res: Response) => Promise<Response<any, Record<string, any>> | undefined>;
-export declare const updateFeedback: (req: AuthenticatedRequest, res: Response) => Promise<Response<any, Record<string, any>> | undefined>;
-export declare const deleteFeedback: (req: AuthenticatedRequest, res: Response) => Promise<Response<any, Record<string, any>> | undefined>;
-export declare const getFeedbackComments: (req: AuthenticatedRequest, res: Response) => Promise<Response<any, Record<string, any>> | undefined>;
-export declare const addComment: (req: AuthenticatedRequest, res: Response) => Promise<Response<any, Record<string, any>> | undefined>;
--- backend/dist/routes/feedbackRoutes.d.ts
+++
@@ -1,2 +0,0 @@
-declare const feedbackRouter: import("express-serve-static-core").Router;
-export default feedbackRouter;
